e6a12aacf331eb3ec14032c83de74bc6ce315c75
Mònica Ramírez Arceda <monica@probeta.net>
2020-09-23 01:24:12 +0200

    Show results for the search

--- words/views.py
+++ words/views.py
@@ -1,4 +1,11 @@
 from django.shortcuts import render
+from .models import Word
 
 def index(request):
-    return render(request, 'words/index.html')
+    # Get partial or complete word to find
+    search = request.GET.get('search')
+    # Get all words starting with string introduced by user
+    words = list(Word.objects.filter(name__startswith=search))
+    # Send all words to the template
+    context = {'words': words}
+    return render(request, 'words/index.html', context)
