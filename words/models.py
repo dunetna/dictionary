78d21807cba5857d4fefa8c8c208cbc7d8511801
Mònica Ramírez Arceda <monica@probeta.net>
2020-09-22 23:04:05 +0200

    Define dictionary model

--- words/models.py
+++ words/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+class Word(models.Model):
+    name = models.CharField(max_length=100)
+    def __str__(self):
+        return self.name
+    
+class Definition(models.Model):
+    description = models.TextField()
+    # Each description is related to a word.
+    # If the word is removed all descriptions will be removed as well.
+    word = models.ForeignKey('Word', on_delete=models.CASCADE)
+    def __str__(self):
+        return self.description
