8f493b93da3514a5cc54e58bc499631e7d61843f
Mònica Ramírez Arceda <monica@probeta.net>
2020-09-23 10:07:37 +0200

    Fix error: when there is no search, context must be empty

--- words/views.py
+++ words/views.py
@@ -2,12 +2,14 @@ from django.shortcuts import render
 from .models import Definition, Word
 
 def index(request):
+    context = {}
     # Get partial or complete word to find
     search = request.GET.get('search')
-    # Get all words starting with string introduced by user
-    words = list(Word.objects.filter(name__startswith=search))
-    # Send all words to the template
-    context = {'words': words}
+    if search:
+        # Get all words starting with string introduced by user
+        words = list(Word.objects.filter(name__startswith=search))
+        # Send all words to the template
+        context.update(words=words)
     return render(request, 'words/index.html', context)
 
 def show(request, word):    
