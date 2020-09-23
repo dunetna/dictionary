27f7fd96a3ca7a3eabbd5e7bb918acaffaf7a52d
Mònica Ramírez Arceda <monica@probeta.net>
2020-09-23 23:42:07 +0200

    Show class names instead of attribute names to user

--- words/models.py
+++ words/models.py
@@ -1,12 +1,12 @@
 from django.db import models
 
 class Word(models.Model):
-    name = models.CharField(max_length=100, unique=True)
+    name = models.CharField(max_length=100, unique=True, verbose_name="word")
     def __str__(self):
         return self.name
     
 class Definition(models.Model):
-    description = models.TextField()
+    description = models.TextField(verbose_name="definition")
     # Each description is related to a word.
     # If the word is removed all descriptions will be removed as well.
     word = models.ForeignKey('Word', on_delete=models.CASCADE)
