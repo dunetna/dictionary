434aea6ce4db05a98a7bdb8f085128e87e5aa8e1
Mònica Ramírez Arceda <monica@probeta.net>
2020-09-24 22:21:25 +0200

    Order words alphabetically

--- words/models.py
+++ words/models.py
@@ -2,6 +2,8 @@ from django.db import models
 
 class Word(models.Model):
     name = models.CharField(max_length=100, unique=True, verbose_name="word")
+    class Meta:
+        ordering = ['name']
     def __str__(self):
         return self.name
     
